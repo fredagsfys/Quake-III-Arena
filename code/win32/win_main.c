cc6d6b22c955ac57ba133b63e0e98cc5dc17f5f9
Fawk <ok222ax@student.lnu.se>
2015-03-18 23:31:12 +0100

    Some testing mechanics

--- code/win32/win_main.c
+++ code/win32/win_main.c
@@ -1257,7 +1257,71 @@ int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLin
 
 int RunApplication(appConfig config, HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
 	// DO SETUP OF SYSTEM BEFORE WINMAIN
-	
+	char		cwd[MAX_OSPATH];
+	int			startTime, endTime;
+
+	// should never get a previous instance in Win32
+	if (hPrevInstance) {
+		return 0;
+	}
+
+	g_wv.hInstance = hInstance;
+	Q_strncpyz(sys_cmdline, lpCmdLine, sizeof(sys_cmdline));
+
+	// done before Com/Sys_Init since we need this for error output
+	Sys_CreateConsole();
+
+	// no abort/retry/fail errors
+	SetErrorMode(SEM_FAILCRITICALERRORS);
+
+	// get the initial time base
+	Sys_Milliseconds();
+#if 0
+	// if we find the CD, add a +set cddir xxx command line
+	Sys_ScanForCD();
+#endif
+
+	Sys_InitStreamThread();
+
+	Com_Init(sys_cmdline);
+	NET_Init();
+
+	_getcwd(cwd, sizeof(cwd));
+	Com_Printf("Working directory: %s\n", cwd);
+
+	// hide the early console since we've reached the point where we
+	// have a working graphics subsystems
+	if (!com_dedicated->integer && !com_viewlog->integer) {
+		Sys_ShowConsole(0, qfalse);
+	}
+
+	// main game loop
+	while (1) {
+		// if not running as a game client, sleep a bit
+		if (g_wv.isMinimized || (com_dedicated && com_dedicated->integer)) {
+			Sleep(5);
+		}
+
+		// set low precision every frame, because some system calls
+		// reset it arbitrarily
+		//		_controlfp( _PC_24, _MCW_PC );
+		//    _controlfp( -1, _MCW_EM  ); // no exceptions, even if some crappy
+		// syscall turns them back on!
+
+		startTime = Sys_Milliseconds();
+
+		// make sure mouse and joystick are only called once a frame
+		IN_Frame();
+
+		// run the game
+		Com_Frame();
+
+		endTime = Sys_Milliseconds();
+		totalMsec += endTime - startTime;
+		countMsec++;
+	}
+
+	// never gets here
 	//
-	WinMain(hInstance, hPrevInstance, lpCmdLine, nCmdShow);
+	//WinMain(hInstance, hPrevInstance, lpCmdLine, nCmdShow);
 }
--- q3_test/tests.c
+++ q3_test/tests.c
@@ -2,10 +2,43 @@
 #include "fff.h"
 #include "unity.h"
 
+
 int RunApplication(appConfig config, HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow);
+
+void test(void) {
+
+	/*Sys_CreateConsole();
+	Com_InitSmallZoneMemory();
+	Cbuf_Init();
+	Com_InitZoneMemory();
+	Cmd_Init();
+	Cbuf_AddText("echo hej");
+	Cbuf_Execute();
+	while (1) {}*/
+}
+
 int WINAPI WinMain2(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
 	appConfig config = { CONNECT_FAIL };
+
+	int threadId;
+
+	/*HANDLE h = CreateThread(
+		NULL,	// LPSECURITY_ATTRIBUTES lpsa,
+		0,		// DWORD cbStack,
+		(LPTHREAD_START_ROUTINE)test,	// LPTHREAD_START_ROUTINE lpStartAddr,
+		0,			// LPVOID lpvThreadParm,
+		0,			//   DWORD fdwCreate,
+		&threadId);*/
+
 	RunApplication(config, hInstance, NULL, "", SW_SHOW);
+
+	//WaitForSingleObject(h, INFINITE);
+	//HANDLE* h2[1];
+	//h2[0] = h;
+
+	//MsgWaitForMultipleObjects(1, h2, 1, INFINITE, QS_ALLEVENTS);
+
+
 }
 
 
