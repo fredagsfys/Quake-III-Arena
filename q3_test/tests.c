d2466202e85a8a4860be4dcb99dd613ee8cf38d8
Fawk <ok222ax@student.lnu.se>
2015-03-31 00:51:57 +0200

    More testing stuff

--- q3_test/tests.c
+++ q3_test/tests.c
@@ -5,57 +5,31 @@
 
 int RunApplication(appConfig config, HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow);
 void Cbuf_ExecuteText(int exec_when, const char *text);
-void Cmd_Echo_f(void);
-void CL_Connect_f(void);
 void CL_Init(void);
 int checkIfCommandExists(char* command);
-
-void clientFunc(appConfig config) {
-
-	puts("quake3 client started!");
-	puts("");
-	puts("Starting test one, creating command 'echo2' and using it");
-
-	/*Cmd_RemoveCommand(config.commandName);
-	Cmd_AddCommand(config.commandName, config.commandPtr);
-
-	BOOL hej = checkIfCommandExists(config.commandName);
-	if (hej) {
-		Cbuf_ExecuteText(1, "echo2 echo2 was found");
-		puts("Echo2 was found. Test successful!");
-	}*/
-}
+int Sys_MilliSeconds(void);
 
 void serverFunc(appConfig config) {
 
 	puts("quake3 server started!");
 	puts("");
-	puts("Starting test one, creating command 'echo2' and using it");
 
-	Cmd_RemoveCommand(config.commandName);
-	Cmd_AddCommand(config.commandName, config.commandPtr);
+	int start = Sys_MilliSeconds();
+	fputs("Initializing client side...", stdout);
 	CL_Init();
+	printf("Done in %d ms", (Sys_MilliSeconds() - start));
+	puts("");
 
-	BOOL hej = checkIfCommandExists(config.commandName);
-	if (hej) {
-		Cbuf_ExecuteText(1, config.execString);
-		//puts("Echo2 was found. Test successful!");
-	}
-}
+	start = Sys_MilliSeconds();
+	fputs("Connecting client to server...", stdout);
+	Cbuf_ExecuteText(1, config.execString);
+	printf("Done in %d ms", (Sys_MilliSeconds() - start));
+	puts("");
 
-void client(void) {
-
-	appConfig config;
-	config.errorType = CONNECT_FAIL;
-	config.isServer = FALSE;
-	config.ptr = &clientFunc;
-	config.commandName = "echo2";
-	config.commandPtr = &Cmd_Echo_f;
-	config.execString = "echo2 1234123123";
-	config.messageThreadId = 0;
-	config.finished = FALSE;
-
-	RunApplication(config, NULL, NULL, "", SW_SHOW);
+	start = Sys_MilliSeconds();
+	fputs("Setting server map to q3dm7...", stdout);
+	Cbuf_ExecuteText(1, "map q3dm7");
+	printf("Done in %d ms", (Sys_MilliSeconds() - start));
 }
 
 void server(void) {
@@ -64,8 +38,6 @@ void server(void) {
 	config.errorType = CONNECT_FAIL;
 	config.isServer = TRUE;
 	config.ptr = &serverFunc;
-	config.commandName = "connect";
-	config.commandPtr = &CL_Connect_f;
 	config.execString = "connect 127.0.0.1";
 	config.messageThreadId = 0;
 	config.finished = FALSE;
@@ -91,11 +63,6 @@ void startServer(void) {
 	HANDLE serverHandle = startThread(server, &threadId);
 }
 
-void startClient(void) {
-	int threadId;
-	HANDLE clientHandle = startThread(client, &threadId);
-}
-
 int equals(char* c1, char* c2) {
 	if (!c1 || !c2) return 0;
 	int i = 0;
@@ -113,21 +80,14 @@ int equals(char* c1, char* c2) {
 
 int main(void) {
 
-	puts("Starting quake3 environment...");
-
+	puts("--- Starting quake3 environment.. ---");
+	puts("");
+	startServer();
 	char* line[1024];
 
 	do {
 
 		scanf("%s", line);
-
-		if (equals(line, "client")) {
-			startClient();
-		}
-
-		if (equals(line, "server")) {
-			startServer();
-		}
 
 	} while (!equals(line, "exit"));
 }
