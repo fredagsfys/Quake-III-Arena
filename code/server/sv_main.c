b74246822aa68a05ef1f00a0ac9955f195a1d20f
Haris <devhariis@gmail.com>
2015-04-20 13:23:01 +0200

    Latest

--- code/server/sv_main.c
+++ code/server/sv_main.c
@@ -185,7 +185,8 @@ void QDECL SV_SendServerCommand(client_t *cl, const char *fmt, ...) {
 	// send the data to all relevent clients
 	for (j = 0, client = svs.clients; j < sv_maxclients->integer ; j++, client++) {
 		if ( client->state < CS_PRIMED ) {
-			continue;
+			/*continue*/;
+			// OSKAR FIX
 		}
 		SV_AddServerCommand( client, (char *)message );
 	}
